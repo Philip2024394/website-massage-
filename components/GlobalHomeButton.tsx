705ea35a02d58d299b435a598284438322476665
Philip2024394 <Philip2024394@users.noreply.github.com>
2025-11-21 13:03:18 +0700

    chore(ui): move global Home button to top-right in header

--- components/GlobalHomeButton.tsx
+++ components/GlobalHomeButton.tsx
@@ -13,7 +13,7 @@ const GlobalHomeButton: React.FC<Props> = ({ onClick, hidden }) => {
       type="button"
       onClick={onClick}
       aria-label="Home"
-      className="fixed top-4 left-4 z-[10000] w-12 h-12 bg-orange-600 rounded-full shadow-lg hover:shadow-xl flex items-center justify-center hover:scale-110 transition-all hover:bg-orange-700"
+      className="fixed top-4 right-4 z-[10000] w-12 h-12 bg-orange-600 rounded-full shadow-lg hover:shadow-xl flex items-center justify-center hover:scale-110 transition-all hover:bg-orange-700"
       style={{ WebkitTapHighlightColor: 'transparent' } as React.CSSProperties}
     >
       <Home className="w-6 h-6 text-white" />
