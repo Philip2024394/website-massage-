823b018b14b633a06910c424bf948d75f04dd9c5
Philip2024394 <Philip2024394@users.noreply.github.com>
2026-02-09 19:27:56 +0700

    update: refresh service worker build artifacts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b042cce6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bccma7m95c"
+    "revision": "0.fl6tou8ig8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
